3edf7f19db4244fb8727e29b8c0fed950602a3eb
Felix Stürmer <weltenwort@users.noreply.github.com>
2021-09-17 20:41:30 +0200

    Add calculated entities for power generation (#127)

---
+++ custom_components/rct_power/lib/state_helpers.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.typing import StateType
+
+from .api import ApiResponseValue
+from .const import NUMERIC_STATE_DECIMAL_DIGITS
+
+
+def get_first_api_response_value_as_state(
+    entity: SensorEntity,
+    values: list[Optional[ApiResponseValue]],
+) -> StateType:
+    if len(values) <= 0:
+        return None
+
+    return get_api_response_value_as_state(entity=entity, value=values[0])
+
+
+def get_api_response_value_as_state(
+    entity: SensorEntity,
+    value: Optional[ApiResponseValue],
+) -> StateType:
+    if isinstance(value, bytes):
+        return value.hex()
+
+    if isinstance(value, tuple):
+        return None
+
+    if isinstance(value, (int, float)) and entity.native_unit_of_measurement == "%":
+        return round(value * 100, NUMERIC_STATE_DECIMAL_DIGITS)
+
+    if isinstance(value, (int, float)):
+        return round(value, NUMERIC_STATE_DECIMAL_DIGITS)
+
+    return value
+
+
+def get_first_api_reponse_value_as_absolute_state(
+    entity: SensorEntity,
+    values: list[Optional[ApiResponseValue]],
+) -> StateType:
+    value = get_first_api_response_value_as_state(entity=entity, values=values)
+
+    if isinstance(value, (int, float)):
+        return abs(value)
+
+    return value
+
+
+def sum_api_response_values_as_state(
+    entity: SensorEntity,
+    values: list[Optional[ApiResponseValue]],
+) -> StateType:
+    return sum(value for value in values if isinstance(value, (int, float)))
