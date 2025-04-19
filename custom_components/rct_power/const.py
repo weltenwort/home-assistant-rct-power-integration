802d1019512de28b69b0e0d2486e4e79cfac07d5
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2025-04-19 14:26:57 +0200

    Improve ConfigFlow (#468)
    * Improve ConfigFlow
    
    * Fix timedelta calls
    
    * Use dedicated enums for the ScanInterval conf value and default
    
    * Improve typing for entry.data and entry.options

--- custom_components/rct_power/const.py
+++ custom_components/rct_power/const.py
@@ -1,5 +1,26 @@
 from __future__ import annotations
 
 import logging
+from enum import IntEnum, StrEnum
+from typing import Final
 
 LOGGER = logging.getLogger(__package__)
+
+# Configuration, options, defaults
+CONF_ENTITY_PREFIX: Final = "entity_prefix"
+CONF_HOSTNAME: Final = "hostname"
+
+DEFAULT_ENTITY_PREFIX: Final = "RCT Power Storage"
+DEFAULT_PORT: Final = 8899
+
+
+class ConfScanInterval(StrEnum):
+    FREQUENT = "frequent_scan_interval"
+    INFREQUENT = "infrequent_scan_interval"
+    STATIC = "static_scan_interval"
+
+
+class ScanIntervalDefault(IntEnum):
+    FREQUENT = 30
+    INFREQUENT = 60 * 3
+    STATIC = 60 * 60
